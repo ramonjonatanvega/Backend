200c722a77667039dff04b90ff6ac7da9bfc5dcc
jonatan <ramonjonatanvega@outlook.com>
2023-02-26 18:30:29 -0300

    correccion rutas renderr

--- src/main/java/com/portfolio/ArgentinaPrograma/controller/PersonaController.java
+++ src/main/java/com/portfolio/ArgentinaPrograma/controller/PersonaController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("persona")
-@CrossOrigin(origins= "https://portfolio-ramon.web.app/index")
+@CrossOrigin(origins= "https://portfolio-ramon.web.app")
 public class PersonaController {
   
         @Autowired
--- src/main/java/com/portfolio/ArgentinaPrograma/controller/SkillController.java
+++ src/main/java/com/portfolio/ArgentinaPrograma/controller/SkillController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("skill")
-@CrossOrigin(origins= "https://portfolio-ramon.web.app/index")
+@CrossOrigin(origins= "https://portfolio-ramon.web.app")
 public class SkillController {
    
      @Autowired
